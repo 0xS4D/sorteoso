8dac4d5f7fa8d766ccee4a493d6005cef91d64a6
EtZeta <heavenzeta@gmail.com>
2023-08-01 04:44:18 -0300

    Minimal fix on background

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Form from './components/Form'
 
 export default function App() {
   return (
-    <div className='h-screen w-screen bg-cover sm:bg-bottom flex flex-col items-center p-5 sm:p-10 bg-[url("/img/bg.png")] bg-[0%_-3em]'>
+    <div className='h-screen w-screen bg-cover sm:bg-bottom flex flex-col items-center p-5 sm:p-10 bg-[url("/img/bg.png")] bg-[0%_-6em]'>
       <Logo className='w-80 sm:w-60 md:w-72 lg:w-84 xl:w-96 mb-5' />
       <Form />
     </div>

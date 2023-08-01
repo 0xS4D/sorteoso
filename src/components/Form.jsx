e7069c78e58e389b7e1a74e7f3313a250d72383a
EtZeta <heavenzeta@gmail.com>
2023-08-01 04:33:57 -0300

    Fix responsive

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,39 +12,39 @@ export default function Form() {
   const totalTickets = useMemo(() => listaParticipantes.reduce((total, p) => total + p.qty, 0), [listaParticipantes])
 
   function sortear() {
-    const sorteo = Math.floor(Math.random() * totalTickets) + 1;
-    let acumulador = 0;
-  
+    const sorteo = Math.floor(Math.random() * totalTickets) + 1
+    let acumulador = 0
+
     for (let i = 0; i < listaParticipantes.length; i++) {
-      acumulador += listaParticipantes[i].qty;
+      acumulador += listaParticipantes[i].qty
       if (sorteo <= acumulador) {
-        const ganadorActualizado = listaParticipantes[i];
-        setGanador(ganadorActualizado);
+        const ganadorActualizado = listaParticipantes[i]
+        setGanador(ganadorActualizado)
         setHistorico((prevHistorico) => {
           if (prevHistorico[indicePremio]) {
-            const ganadorAnterior = prevHistorico[indicePremio].ganador;
+            const ganadorAnterior = prevHistorico[indicePremio].ganador
             setListaParticipantes((prevParticipantes) => {
               return prevParticipantes.map((participante) => {
                 if (participante.id === ganadorAnterior.id) {
-                  return {...participante, qty: participante.qty + 1};
+                  return {...participante, qty: participante.qty + 1}
                 }
-                return participante;
-              });
-            });
+                return participante
+              })
+            })
           }
-          const newHistorico = [...prevHistorico];
-          newHistorico[indicePremio] = {ganador: ganadorActualizado, premio: premios[indicePremio]};
-          return newHistorico;
-        });
+          const newHistorico = [...prevHistorico]
+          newHistorico[indicePremio] = {ganador: ganadorActualizado, premio: premios[indicePremio]}
+          return newHistorico
+        })
         setListaParticipantes((prevParticipantes) => {
           return prevParticipantes.map((participante) => {
             if (participante.id === ganadorActualizado.id) {
-              return {...participante, qty: participante.qty - 1};
+              return {...participante, qty: participante.qty - 1}
             }
-            return participante;
-          });
-        });
-        break;
+            return participante
+          })
+        })
+        break
       }
     }
   }
@@ -72,19 +72,23 @@ export default function Form() {
   }
 
   return (
-    <div className='relative bg-[#ffffff11] backdrop-blur-md border-[5px] h-[55%] border-orange-500 w-full sm:h-auto sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 rounded-3xl p-5 flex flex-col items-center justify-between'>
-      <div className='absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-orange-500 text-white font-bold p-1 rounded-bl-lg'>
-        #{premios[indicePremio].id}
+    <>
+      <div className='relative bg-[#ffffff11] backdrop-blur-md border-[5px] border-orange-500 w-full sm:h-auto sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 rounded-3xl px-5 flex flex-col items-center justify-between'>
+        <div className='absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-orange-500 text-white font-bold p-1 rounded-bl-lg'>
+          #{premios[indicePremio].id}
+        </div>
+        <div className='flex flex-col items-center h-full space-y-2 pt-2'>
+          <h1 className='font-bold text-4xl sm:text-5xl md:text-6xl text-white'>Ganador: </h1>
+          <p className='font-semibold text-3xl sm:text-4xl md:text-5xl text-orange-300 text-center'>{ganador ? ganador.participante : 'N/A'}</p>
+          <h2 className='font-semibold text-2xl sm:text-3xl md:text-4xl text-white'>Premio:</h2>
+          <p className='font-medium text-2xl pb-2 sm:text-3xl md:text-4xl text-orange-300 text-center'>{premios[indicePremio].premio}</p>
+          <div className='flex flex-col items-end justify-around w-full'>
+            <p className='font-medium text-sm sm:text-base md:text-lg text-white'>Gracias a:</p>
+            <p className='font-medium text-sm sm:text-base md:text-lg mb-5 text-white'>{premios[indicePremio].auth}</p>
+          </div>
+        </div>
       </div>
-      <div className='flex flex-col items-center h-full space-y-2 pt-10'>
-        <h1 className='font-bold text-4xl sm:text-5xl md:text-6xl text-white'>Ganador: </h1>
-        <p className='font-semibold text-3xl sm:text-4xl md:text-5xl text-orange-300 text-center'>{ganador ? ganador.participante : 'N/A'}</p>
-        <h2 className='font-semibold text-2xl sm:text-3xl md:text-4xl text-white'>Premio:</h2>
-        <p className='font-medium text-2xl pb-2 sm:text-3xl md:text-4xl text-orange-300 text-center'>{premios[indicePremio].premio}</p>
-      </div>
-      <div className='flex flex-col items-end justify-around w-full'>
-        <p className='font-medium text-sm sm:text-base md:text-lg text-white'>Gracias a:</p>
-        <p className='font-medium text-sm sm:text-base md:text-lg mb-5 text-white'>{premios[indicePremio].auth}</p>
+      <div className='flex flex-col items-end w-64 pt-3'>
         <button
           className='bg-black text-white font-bold w-full flex justify-center items-center h-10 rounded-2xl cursor-pointer hover:bg-orange-500 mb-5'
           onClick={sortear}
@@ -107,6 +111,6 @@ export default function Form() {
           </button>
         </div>
       </div>
-    </div>
+    </>
   )
 }
